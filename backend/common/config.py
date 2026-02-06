d0aab2acf09b64b9e43f9db885a34a6d2229cc3a
Osanda Muthukumarana <it22170798@my.sliit.lk>
2026-02-07 02:06:28 +0530

    setup the database config -- dev

---
+++ backend/common/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# backend db config
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    APP_NAME: str = "Stock Platform Backend"
+    ENV: str = "dev"
+    DATABASE_URL: str = ""
+
+
+
+settings = Settings()  
